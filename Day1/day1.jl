362dbef7a1462b45bed7d4dcfd719543a94e3d1a
Tristan Gray-Davies <tgraydavies@gmail.com>
2021-12-01 23:26:04 +0000

    Tidy

--- Day1/day1.jl
+++ Day1/day1.jl
@@ -1,8 +1,7 @@
-data = readlines("data.txt");
-data = map(x->parse(Int,x),data);
-sumIncreases = x->sum(x[2:end].>x[1:end-1]);
-print("Number of increases = "*string(sumIncreases(data))*"\n")
+data = map(x->parse(Int,x),readlines("data.txt"));
+nInc = x->sum(x[2:end].>x[1:end-1]);
+print("Answer 1 = $(nInc(data))\n")
 
 rolSum = cumsum(data);
 rolSum = rolSum[3:end]-[0; rolSum[1:end-3]];
-print("Number of increases in rolling sum = "*string(sumIncreases(rolSum)))
+print("Answer 2 = $(nInc(rolSum))\n")
