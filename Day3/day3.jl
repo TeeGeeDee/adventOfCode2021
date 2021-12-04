b7d41e40a74caf4f3d8d8e799e21757b2b3f0bf7
Tristan Gray-Davies <tgraydavies@gmail.com>
2021-12-04 18:01:24 +0000

    tidy

--- Day3/day3.jl
+++ Day3/day3.jl
@@ -1,23 +1,23 @@
-file = "data.txt";
-freq = [[c=='1' for c in s] for s in eachline(file)];
+data = readlines(if isempty(ARGS) "data.txt" else ARGS[1] end);
+freq = [[c=='1' for c in s] for s in data];
 gammabin = sum(freq).>(length(freq)/2);
 bin2dec(binarray) = parse(Int,reduce(*,map(x->string(Int(x)),binarray)),base=2)
-gamma = bin2dec(gammabin)
+gamma   = bin2dec(gammabin)
 epsilon = bin2dec(.!gammabin)
 print("Answer 1 = $(gamma*epsilon)\n")
 
-function applyrule(mydata,incond)
+function applyrule(mydata,inrule)
     pos = 0;
-    while (pos<length(mydata[1])) & (length(mydata)>1)
+    while length(mydata)>1
         pos += 1;
-        mydata = mydata[incond([d[pos] for d in mydata])]
+        mydata = mydata[inrule([d[pos] for d in mydata])]
     end
     return parse(Int,mydata[1],base=2)
 end
 
-oxygenincluded(x) = x.== if sum(x.=='1')>=(length(x)/2) '1' else '0' end
-co2included(x) = .!oxygenincluded(x)
-oxygen = applyrule(readlines(file),oxygenincluded)
-co2 = applyrule(readlines(file),co2included)
+oxygenrule(x) = x.== if sum(x.=='1')>=(length(x)/2) '1' else '0' end;
+co2rule(x) = .!oxygenrule(x);
+oxygen = applyrule(data,oxygenrule);
+co2    = applyrule(data,co2rule);
 print("Answer 2 = $(oxygen*co2)\n")
 
