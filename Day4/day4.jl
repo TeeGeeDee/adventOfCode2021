d8214e8c30b98ee794cc3c195253a0c6bbbdd17a
Tristan Gray-Davies <tgraydavies@gmail.com>
2021-12-04 20:38:39 +0000

    tidy

--- Day4/day4.jl
+++ Day4/day4.jl
@@ -1,27 +1,21 @@
 data = readlines(if isempty(ARGS) "data.txt" else ARGS[1] end);
 callouts = map(x->parse(Int,x),split(data[1],","));
 nboards  = Int((length(data)-1)/6);
-boards   = Vector{Matrix}(undef,nboards);
-hits     = Vector{Matrix}(undef,nboards);
-for i in 1:nboards
-    boards[i] = parse.(Int,reduce(hcat,split.(data[3+(i-1)*6:i*6+1])));
-    hits[i]   = falses(5,5);
-end
+boards = [parse.(Float64,reduce(hcat,split.(data[3+(i-1)*6:i*6+1]))) for i in 1:nboards];
 finished = falses(nboards);
-
 for callednum in callouts, i in 1:length(boards)
-    hits[i][boards[i].==callednum] .= true;
-    if !finished[i] & (any(all(hits[i],dims=1)) | any(all(hits[i],dims=2)))
-        bingo = callednum*sum(boards[i][.!hits[i]]);
+    if finished[i] continue end
+    boards[i][boards[i].==callednum] .= NaN; # mark the board
+    hits = isnan.(boards[i]);
+    if !finished[i] & (any(all(hits,dims=1)) | any(all(hits,dims=2)))
+        score = callednum * sum(Int.(boards[i][.!hits]));
         if !any(finished)
-            print("Winning score = $bingo\n");
+            print("Part 1 score = $score\n");
         end
         finished[i] = true;
         if all(finished)
-            print("Losing score = $bingo\n");
+            print("Part 2 score = $score\n");
             break
         end
-
     end
 end
-
