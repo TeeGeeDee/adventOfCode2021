c1bfede6fb8fbd2a8ba505b31fa7a23bad29bb1b
Tristan Gray-Davies <tgraydavies@gmail.com>
2021-12-05 17:56:19 +0000

    tidy diagonal indexing

--- Day5/day5.jl
+++ Day5/day5.jl
@@ -1,18 +1,18 @@
 function day5(file)
     data = split.(replace.(readlines(file)," -> " => ","),",");
     data = parse.(Int,reduce(hcat,data))' .+ 1; # 0 indexing -> 1 indexing
+    atob(x,y) = x:(x<y ? 1 : -1):y;
 
     function countoverlaps(linedata,includediagonal)
-        grid = zeros(maximum(linedata),maximum(linedata));
+        gridsize = maximum(linedata);
+        grid = zeros(gridsize,gridsize);
         for (x1,y1,x2,y2) in eachrow(linedata)
             if x1==x2
-                grid[x1,range(sort([y1,y2])...)] .+= 1
+                grid[x1,atob(y1,y2)] .+= 1
             elseif y1==y2
-                grid[range(sort([x1,x2])...),y1] .+= 1
+                grid[atob(x1,x2),y1] .+= 1
             elseif includediagonal
-                xinc = x1<x2 ? 1 : -1
-                yinc = y1<y2 ? 1 : -1
-                grid[[CartesianIndex(x1+i*xinc,y1+i*yinc) for i = 0:abs(x1-x2)]] .+= 1
+                grid[CartesianIndex.(zip(atob(x1,x2),atob(y1,y2)))] .+= 1
             end
         end
         return sum(grid.>1)
