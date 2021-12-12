42edf7c0ca0c804cd9d2c7872515e96768300c8a
Tristan Gray-Davies <tgraydavies@gmail.com>
2021-12-12 22:35:57 +0000

    Tidy day 12

--- Day12/day12.jl
+++ Day12/day12.jl
@@ -2,30 +2,25 @@ function day12(file)
     moves = Dict{String,Set{String}}();
     addpath!(a,b,d::Dict) = haskey(d,a) ? push!(d[a],b) :
                                           d[a] = Set{String}((b,));
-    for  (a,b) in split.(eachline(file),"-")
+    for (a,b) in split.(eachline(file),"-")
         addpath!(a,b,moves);
         addpath!(b,a,moves);
     end
-    for k in keys(moves) moves[k] = setdiff(moves[k],("start",)) end # don't go back to start
+    for k in keys(moves) setdiff!(moves[k],("start",)); end # don't go back to start
 
-    function numpathsfrom(position,visitedsmallcaves,isonevisitonly)
-        if position=="end" return 1 end
-        if all(islowercase.(collect(position)))
-            beenbefore = position in visitedsmallcaves;
-            if isonevisitonly && beenbefore
-                return 0
-            elseif beenbefore
-                isonevisitonly = true; # from now on one visit only
-            else
-                push!(visitedsmallcaves,position);
+    function numpathsfrom(cave,visitedsmallcaves,isonevisitonly)
+        if cave=="end" return 1 end
+        if all(islowercase.(collect(cave)))
+            beenbefore = cave in visitedsmallcaves;
+            if isonevisitonly && beenbefore  return 0
+            elseif beenbefore                isonevisitonly = true; # from now on one visit only
+            else                             push!(visitedsmallcaves,cave);
             end
         end
-        next = moves[position];
-        if isonevisitonly
-            next = setdiff(next,visitedsmallcaves);
-        end
-        if isempty(next) return 0 end
-        return sum(numpathsfrom(p,copy(visitedsmallcaves),isonevisitonly) for p in next)
+        next = moves[cave];
+        if isonevisitonly next = setdiff(next,visitedsmallcaves); end
+        if isempty(next)  return 0 end
+        return sum(numpathsfrom(c,copy(visitedsmallcaves),isonevisitonly) for c in next)
     end
     return numpathsfrom("start",Set{String}(),true),numpathsfrom("start",Set{String}(),false)
 end
